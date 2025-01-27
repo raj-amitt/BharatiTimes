3e1cd0cb7055962c6478c9b948d64a4996d40d69
Lakshya Rana <work.lakshyarana@gmail.com>
2025-01-27 12:55:11 +0530

    home page finalized

---
+++ frontend/src/components/FullCard.jsx
@@ -0,0 +1,25 @@
+const FullCard = () => (
+    <div className="relative rounded-lg overflow-hidden bg-white text-white h-86">
+      {/* Background Image */}
+      <img
+        src="../../tech.jpg"
+        className="absolute inset-0 w-full h-full object-cover"
+        alt=""
+      />
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-black opacity-40"></div>
+      {/* Content */}
+      <div className="absolute bottom-0 flex flex-col justify-center gap-4 !p-6">
+        <p className="text-xs md:text-sm">Author | 3 hours ago</p>
+        <h3 className="text-xl md:text-3xl font-medium">
+          Lorem ipsum dolor sit amet consectetur
+        </h3>
+        <p className="text-xs md:text-sm">
+          <span className="text-red-400 font-medium">Category</span> | 8 min read
+        </p>
+      </div>
+    </div>
+  );
+  
+  export default FullCard;
+  
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import MidCard from "../components/MidCard";
 import SmallCard from "../components/SmallCard";
 import BigCard from "../components/BigCard";
 import SideCard from "../components/SideCard";
+import FullCard from "../components/FullCard";
 import AdSlider from "../components/AdSlider";
 
 const Home = () => (
@@ -9,12 +10,13 @@ const Home = () => (
     <div className="flex gap-4 !px-6 !py-10 bg-white">
       <main className="flex flex-col gap-8 md:gap-10">
         {/* Top News */}
-        <section className="flex flex-col md:flex-row gap-10">
+        <section className="flex flex-col md:flex-row md:items-center gap-10">
           <div className="w-full md:w-3/5">
             <BigCard />
           </div>
           <div className="w-full md:w-2/5">
             <div className="flex flex-col gap-8 md:gap-5">
+              <h2 className="text-xl font-medium">National News</h2>
               <SideCard />
               <SideCard />
             </div>
@@ -43,7 +45,7 @@ const Home = () => (
         <section className="flex flex-col md:flex-row gap-10">
           {/* National News */}
           <div className="flex flex-col gap-5">
-            <h2 className="text-xl font-medium">National News</h2>
+            <h2 className="text-xl font-medium">International News</h2>
             <div className="grid grid-cols-2 md:grid-cols-3 gap-5">
               <SmallCard />
               <SmallCard />
@@ -54,13 +56,61 @@ const Home = () => (
           </div>
           {/* International News */}
           <div className="flex flex-col gap-5">
-            <h2 className="text-xl font-medium">International News</h2>
+            <h2 className="text-xl font-medium">Science & Tech News</h2>
             <div className="grid grid-cols-2 md:grid-cols-3 gap-5">
               <SmallCard />
               <SmallCard />
               <div className="hidden md:block">
                 <SmallCard />
               </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="flex flex-col md:flex-row md:items-center gap-10">
+          <div className="w-full md:w-3/5 flex flex-col gap-8 md:gap-5">
+            <h2 className="text-xl font-medium">Business & Economics News</h2>
+            <FullCard />
+          </div>
+          <div className="w-full md:w-2/5">
+            <div className="flex flex-col gap-8 md:gap-5">
+              <h2 className="text-xl font-medium">Education News</h2>
+              <SideCard />
+              <SideCard />
+            </div>
+          </div>
+        </section>
+
+        <section className="flex flex-col md:flex-row gap-10">
+          {/* Education News */}
+          <div className="flex flex-col gap-5 md:w-1/2">
+            <h2 className="text-xl font-medium">Health News</h2>
+            <div className="grid grid-cols-2 gap-5">
+              <MidCard />
+              <MidCard />
+            </div>
+          </div>
+          {/* Health News */}
+          <div className="flex flex-col gap-5 md:w-1/2">
+            <h2 className="text-xl font-medium">Entertainment News</h2>
+            <div className="grid grid-cols-2 gap-5">
+              <MidCard />
+              <MidCard />
+            </div>
+          </div>
+        </section>
+
+        <section className="flex flex-col md:flex-row gap-10">
+          {/* Sports News */}
+          <div className="flex flex-col gap-5">
+            <h2 className="text-xl font-medium">Sports News</h2>
+            <div className="grid grid-cols-2 md:grid-cols-6 gap-5">
+              <SmallCard />
+              <SmallCard />
+              <SmallCard />
+              <SmallCard />
+              <SmallCard />
+              <SmallCard />
             </div>
           </div>
         </section>
